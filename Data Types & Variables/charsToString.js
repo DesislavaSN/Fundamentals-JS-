c3f47cb3386c2bd6c6cf16382ad8b2fa6aeb8ebf
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-05-04 09:10:11 +0100

    Create charsToString.js

---
+++ Data Types & Variables/charsToString.js
@@ -0,0 +1,6 @@
+// A function which combinse all the characters into one string.
+
+function charsToString(param1, param2, param3){
+    let res ="" + param1 + param2 + param3;
+    console.log(res);
+}
