3dbdb74dc65dbca3f80653da9bceddfa98610aaa
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-05-04 09:13:51 +0100

    Create townInfo.js

---
+++ Data Types & Variables/townInfo.js
@@ -0,0 +1,5 @@
+// A function which prints a town information from the given parameters.
+
+function townInfo(townName, population , areaNum){
+    console.log(`Town ${townName} has population of ${population} and area ${areaNum} square km.`);
+}
