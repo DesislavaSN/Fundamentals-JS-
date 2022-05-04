e212ec499f105d9a4949533dcdaa1ea0c9d2295c
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-05-04 09:17:55 +0100

    Create convertMetersToKm.js

---
+++ Data Types & Variables/convertMetersToKm.js
@@ -0,0 +1,12 @@
+function convertMetersToKm(meters){
+    let oneKm = 0.001;
+    let meterToKm = meters * oneKm;  
+    console.log(meterToKm.toFixed(2));
+}
+
+
+// 2nd option:
+// function convertMetersToKm(meters){
+//    let meterToKm = meters / 1000;
+//    console.log(meterToKm.toFixed(2));
+// }
