293601b59072735fe8c504f322d412e25e6037cb
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-05-09 09:12:38 +0100

    Create signCheck.js

---
+++ Functions/signCheck.js
@@ -0,0 +1,9 @@
+// A function, that checks whether the result of the multiplication numOne * numTwo * numThree is positive or negative.
+
+function signCheck(num1, num2, num3) {
+    if (num1 * num2 * num3 > 0) {
+        return ("Positive");
+    } else {
+        return ("Negative");
+    }
+}
