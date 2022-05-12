5d0b9d4bb9725c1d1fd5b53dcfb898f57d5fba3e
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-05-12 09:47:17 +0100

    Delete Assiciative Arrays/Text Processing directory

--- Assiciative Arrays/Text Processing/2.Substring.js
+++
@@ -1,9 +0,0 @@
-//2.	Substring
-
-function substring(str, start, count){
-    return str.substring(start, count + start);
-}
-
-console.log(substring('ASentence', 1, 8));//Sentence
-console.log(substring('SkipWord', 4, 7) );// Word
-console.log(substring('JavaScript is alsome!!!', 4, 12) );// Script is al
